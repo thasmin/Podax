f771876b175cbf865edb07e59bcf95bf9341d33b
Dan Goldstein <dan@axelby.com>
2011-05-06 21:50:09 -0400

    moved Google Reader import to background thread

--- src/com/axelby/podax/GoogleAccountChooserActivity.java
+++ src/com/axelby/podax/GoogleAccountChooserActivity.java
@@ -12,6 +12,7 @@ import android.accounts.AccountManagerFuture;
 import android.accounts.AuthenticatorException;
 import android.accounts.OperationCanceledException;
 import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.sax.Element;
 import android.sax.ElementListener;
@@ -48,86 +49,102 @@ public class GoogleAccountChooserActivity extends ListActivity {
 	}
 
 	@Override
-	protected void onListItemClick(ListView l, View v, int position, long itemId) {
-		Account account = _accounts[position];
-		HttpTransport transport = GoogleTransport.create();
-		GoogleHeaders headers = (GoogleHeaders) transport.defaultHeaders;
-		headers.setApplicationName("Podax");
-		headers.gdataVersion = "2";
-		
-		AccountManagerFuture<Bundle> accountManagerFuture = _accountManager.getAuthToken(account, "reader", true, null, null);
-		try {
-			Bundle authTokenBundle = accountManagerFuture.getResult();
-			String authToken = authTokenBundle.get(AccountManager.KEY_AUTHTOKEN).toString();
-			headers.setGoogleLogin(authToken);
-			HttpRequest request = transport.buildGetRequest();
-			request.url = new GenericUrl("http://www.google.com/reader/api/0/subscription/list");
-			InputStream response;
-			try {
-				response = request.execute().getContent();
-			} catch (HttpResponseException ex) {
-				_accountManager.invalidateAuthToken("reader", authToken);
-				authToken = authTokenBundle.get(AccountManager.KEY_AUTHTOKEN).toString();
-				headers.setGoogleLogin(authToken);
-				request = transport.buildPostRequest();
-				request.url = new GenericUrl("http://www.google.com/reader/api/0/subscription/list");
-				response = request.execute().getContent();
-			}
-			
-			// set up the sax parser
-			RootElement root = new RootElement("object");
-			Element object = root.getChild("list").getChild("object");
-			// put these in an array to we can use them in the inner class
-			final String[] id = { "" };
-			final String[] title = { "" };
-			final String[] stringType = { "" };
-			final DBAdapter adapter = DBAdapter.getInstance(this);
-			object.setElementListener(new ElementListener() {
+	protected void onListItemClick(ListView l, View v, final int position, long itemId) {
+		getListView().setEnabled(false);
+		Thread thread = new Thread(new Runnable() {
+			public void run() {
+				Account account = _accounts[position];
+				HttpTransport transport = GoogleTransport.create();
+				GoogleHeaders headers = (GoogleHeaders) transport.defaultHeaders;
+				headers.setApplicationName("Podax");
+				headers.gdataVersion = "2";
 
-				public void start(Attributes attrs) {
-					id[0] = "";
-					title[0] = "";
-				}
-
-				public void end() {
-					Subscription subscription = adapter.addSubscription(id[0], title[0]);
-					SubscriptionUpdateService.getInstance().updateSubscription(subscription);
-				}
-				
-			});
-			object.getChild("string").setStartElementListener(new StartElementListener() {
-
-				public void start(Attributes attributes) {
-					stringType[0] = attributes.getValue("name");
-				}
-				
-			});
-			object.getChild("string").setEndTextElementListener(new EndTextElementListener() {
-
-				public void end(String text) {
-					if (stringType[0].equals("id")) {
-						if (text != null && text.startsWith("feed/"))
-							text = text.substring(5);
-						id[0] = text;
+				AccountManagerFuture<Bundle> accountManagerFuture = _accountManager.getAuthToken(account, "reader", true, null, null);
+				try {
+					Bundle authTokenBundle = accountManagerFuture.getResult();
+					String authToken = authTokenBundle.get(AccountManager.KEY_AUTHTOKEN).toString();
+					headers.setGoogleLogin(authToken);
+					HttpRequest request = transport.buildGetRequest();
+					request.url = new GenericUrl("http://www.google.com/reader/api/0/subscription/list");
+					InputStream response;
+					try {
+						response = request.execute().getContent();
+					} catch (HttpResponseException ex) {
+						_accountManager.invalidateAuthToken("reader", authToken);
+						authToken = authTokenBundle.get(AccountManager.KEY_AUTHTOKEN).toString();
+						headers.setGoogleLogin(authToken);
+						request = transport.buildPostRequest();
+						request.url = new GenericUrl("http://www.google.com/reader/api/0/subscription/list");
+						response = request.execute().getContent();
 					}
-					else if (stringType[0].equals("title"))
-						title[0] = text;
+
+					// set up the sax parser
+					RootElement root = new RootElement("object");
+					Element object = root.getChild("list").getChild("object");
+					// put these in an array to we can use them in the inner class
+					final String[] id = { "" };
+					final String[] title = { "" };
+					final String[] stringType = { "" };
+					final DBAdapter adapter = DBAdapter.getInstance(GoogleAccountChooserActivity.this);
+					object.setElementListener(new ElementListener() {
+
+						public void start(Attributes attrs) {
+							id[0] = "";
+							title[0] = "";
+						}
+
+						public void end() {
+							Subscription subscription = adapter.addSubscription(id[0], title[0]);
+							SubscriptionUpdateService.getInstance().updateSubscription(subscription);
+						}
+
+					});
+					object.getChild("string").setStartElementListener(new StartElementListener() {
+
+						public void start(Attributes attributes) {
+							stringType[0] = attributes.getValue("name");
+						}
+
+					});
+					object.getChild("string").setEndTextElementListener(new EndTextElementListener() {
+
+						public void end(String text) {
+							if (stringType[0].equals("id")) {
+								if (text != null && text.startsWith("feed/"))
+									text = text.substring(5);
+								id[0] = text;
+							}
+							else if (stringType[0].equals("title"))
+								title[0] = text;
+						}
+
+					});
+
+					// parse the repsonse
+					Xml.parse(response, Xml.Encoding.UTF_8, root.getContentHandler());
+
+				} catch (OperationCanceledException e) {
+					e.printStackTrace();
+				} catch (AuthenticatorException e) {
+					e.printStackTrace();
+				} catch (IOException e) {
+					e.printStackTrace();
+				} catch (SAXException e) {
+					e.printStackTrace();
 				}
-				
-			});
-			
-			// parse the repsonse
-			Xml.parse(response, Xml.Encoding.UTF_8, root.getContentHandler());
-			Toast.makeText(this, "Google Reader subscriptions imported", Toast.LENGTH_LONG);
-			
-		} catch (OperationCanceledException e) {
-			e.printStackTrace();
-		} catch (AuthenticatorException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (SAXException e) {
+			}
+		});
+		thread.start();
+		try {
+			thread.join();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+
+		Toast.makeText(GoogleAccountChooserActivity.this, "Google Reader subscriptions imported", Toast.LENGTH_LONG);
+		Intent intent = new Intent();
+		intent.setClassName("com.axelby.podax", "com.axelby.podax.SubscriptionListActivity");
+		startActivity(intent);
     }
 }
