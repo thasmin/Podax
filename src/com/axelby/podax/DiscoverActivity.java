7ca7e39b49242a87b837be3836ce81c159fe864b
Dan Goldstein <dan@axelby.com>
2011-11-25 16:36:45 -0500

    added skeleton discover activity

---
+++ src/com/axelby/podax/DiscoverActivity.java
@@ -0,0 +1,15 @@
+package com.axelby.podax;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+
+public class DiscoverActivity extends ListActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.discover);
+	}
+
+}
