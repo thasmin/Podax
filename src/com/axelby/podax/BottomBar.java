a36819e6caa0a7527e7055ffa4bfc1c2cc645cce
Dan Goldstein <dan@axelby.com>
2012-03-25 22:53:54 -0400

    removed spammy debug log message

--- src/com/axelby/podax/BottomBar.java
+++ src/com/axelby/podax/BottomBar.java
@@ -7,7 +7,6 @@ import android.database.Cursor;
 import android.net.Uri;
 import android.os.Handler;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ImageButton;
