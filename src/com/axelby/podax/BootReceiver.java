fde510fd6005c23cd50446362c2cf40bdf150fa5
Dan Goldstein <dan@axelby.com>
2012-05-02 21:06:27 -0400

    avoid showing wifi notice when refresh is triggered by alarm

--- src/com/axelby/podax/BootReceiver.java
+++ src/com/axelby/podax/BootReceiver.java
@@ -5,6 +5,7 @@ import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 
 public class BootReceiver extends BroadcastReceiver {
 
@@ -20,6 +21,8 @@ public class BootReceiver extends BroadcastReceiver {
 		// refresh the feeds
 		Intent refreshIntent = new Intent(context, UpdateService.class);
 		refreshIntent.setAction(Constants.ACTION_REFRESH_ALL_SUBSCRIPTIONS);
+		refreshIntent.setData(Uri.parse("podax://refreshSubscriptions/alarm"));
+		refreshIntent.putExtra(Constants.EXTRA_MANUAL_REFRESH, false);
 		PendingIntent pendingRefreshIntent = PendingIntent.getService(context, 0, refreshIntent, 0);
 		alarmManager.cancel(pendingRefreshIntent);
 		alarmManager.setInexactRepeating(AlarmManager.RTC,
