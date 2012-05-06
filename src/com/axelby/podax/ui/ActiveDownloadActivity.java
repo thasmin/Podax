696e32338f175885163626a9516d4bc34725b6ba
Dan Goldstein <dan@axelby.com>
2012-05-06 17:09:29 -0400

    code style fix

--- src/com/axelby/podax/ui/ActiveDownloadActivity.java
+++ src/com/axelby/podax/ui/ActiveDownloadActivity.java
@@ -34,7 +34,6 @@ public class ActiveDownloadActivity extends SherlockFragmentActivity {
 	    }
 	}
 
-
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 	    MenuInflater inflater = getSupportMenuInflater();
