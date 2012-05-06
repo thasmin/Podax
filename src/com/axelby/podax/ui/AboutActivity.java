8ad0c1e1dfb21e04ac0d8f1a1c37a0e86280593d
Dan Goldstein <dan@axelby.com>
2012-05-06 17:09:49 -0400

    moved about activity to fragment

--- src/com/axelby/podax/ui/AboutActivity.java
+++ src/com/axelby/podax/ui/AboutActivity.java
@@ -1,26 +1,35 @@
 package com.axelby.podax.ui;
 
-import android.app.Activity;
-import android.graphics.Color;
+import android.content.Intent;
 import android.os.Bundle;
-import android.webkit.WebView;
 
+import com.actionbarsherlock.app.SherlockFragmentActivity;
+import com.actionbarsherlock.view.MenuItem;
 import com.axelby.podax.R;
 
-public class AboutActivity extends Activity {
+public class AboutActivity extends SherlockFragmentActivity {
 
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		
-		setContentView(R.layout.about);
-		
-		WebView webview = (WebView) findViewById(R.id.webview);
-		String html = "<html><head><style type=\"text/css\">" +
-				"a { color: #E59F39 }" +
-				"</style></head>" +
-				"<body style=\"background:black;color:white\">" + getString(R.string.about_content) + "</body></html>"; 
-		webview.loadData(html, "text/html", "utf-8");
-		webview.setBackgroundColor(Color.BLACK);
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.about_activity);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+	    switch (item.getItemId()) {
+	        case android.R.id.home:
+	            // app icon in action bar clicked; go home
+	            Intent intent = new Intent(this, MainActivity.class);
+	            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+	            startActivity(intent);
+	            return true;
+	        default:
+	            return super.onOptionsItemSelected(item);
+	    }
 	}
 
 }
---
+++ src/com/axelby/podax/ui/AboutFragment.java
@@ -0,0 +1,41 @@
+package com.axelby.podax.ui;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+
+import com.actionbarsherlock.app.SherlockFragment;
+import com.axelby.podax.R;
+
+public class AboutFragment extends SherlockFragment {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+	        Bundle savedInstanceState) {
+		return inflater.inflate(R.layout.about, container, false);
+	}
+
+
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+		super.onActivityCreated(savedInstanceState);
+
+		WebView webview = (WebView) getActivity().findViewById(R.id.webview);
+		String html = "<html><head><style type=\"text/css\">" +
+				"a { color: #E59F39 }" +
+				"</style></head>" +
+				"<body style=\"background:black;color:white\">" + getString(R.string.about_content) + "</body></html>"; 
+		webview.loadData(html, "text/html", "utf-8");
+		webview.setBackgroundColor(Color.BLACK);
+	}
+
+}
