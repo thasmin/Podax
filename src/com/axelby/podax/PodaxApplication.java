aa6b5c4b82845845e3b4905fa6e303a7461e1096
Dan Goldstein <dan@axelby.com>
2012-07-11 06:22:23 -0400

    add acra for better error reporting

---
+++ src/com/axelby/podax/PodaxApplication.java
@@ -0,0 +1,20 @@
+package com.axelby.podax;
+
+import org.acra.ACRA;
+import org.acra.ReportingInteractionMode;
+import org.acra.annotation.ReportsCrashes;
+
+import android.app.Application;
+
+@ReportsCrashes(formKey = "dFNuZ3FSYW56MXhVVEViV2tpR2xYdEE6MQ", 
+				mode = ReportingInteractionMode.TOAST,
+				resToastText = R.string.crash_toast_text)
+public class PodaxApplication extends Application {
+
+	@Override
+	public void onCreate() {
+		ACRA.init(this);
+		super.onCreate();
+	}
+
+}
