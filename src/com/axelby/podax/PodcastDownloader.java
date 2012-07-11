87e6c6ac8d250dfdea103bcb818e24c686e56ed7
Dan Goldstein <dan@axelby.com>
2012-07-11 06:23:16 -0400

    remove unused imports

--- src/com/axelby/podax/PodcastDownloader.java
+++ src/com/axelby/podax/PodcastDownloader.java
@@ -8,17 +8,9 @@ import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
-import android.app.Notification;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
 import android.content.Context;
-import android.content.Intent;
 import android.database.Cursor;
-import android.support.v4.app.NotificationCompat;
 import android.util.Log;
-
-import com.axelby.podax.R.drawable;
-import com.axelby.podax.ui.MainActivity;
 
 class PodcastDownloader {
 	private Context _context;
