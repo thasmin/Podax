c5b7674cbfcf341214a100584af6efcce58b15bd
Dan Goldstein <dan@axelby.com>
2012-07-21 16:18:07 -0400

    less data from acra but better data hopefully

--- src/com/axelby/podax/PodaxApplication.java
+++ src/com/axelby/podax/PodaxApplication.java
@@ -8,7 +8,16 @@ import android.app.Application;
 
 @ReportsCrashes(formKey = "dFNuZ3FSYW56MXhVVEViV2tpR2xYdEE6MQ", 
 				mode = ReportingInteractionMode.TOAST,
-				resToastText = R.string.crash_toast_text)
+				resToastText = R.string.crash_toast_text,
+				customReportContent = {
+					org.acra.ReportField.USER_COMMENT,
+					org.acra.ReportField.ANDROID_VERSION,
+					org.acra.ReportField.APP_VERSION_NAME,
+					org.acra.ReportField.BRAND,
+					org.acra.ReportField.PHONE_MODEL,
+					org.acra.ReportField.CUSTOM_DATA,
+					org.acra.ReportField.STACK_TRACE,
+				})
 public class PodaxApplication extends Application {
 
 	@Override
