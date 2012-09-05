c9077e27ae801947d9fb75d4401bb832db752d9b
Dan Goldstein <dan@axelby.com>
2012-09-04 20:18:18 -0400

    removed silly preferences

--- src/com/axelby/podax/BluetoothConnectionReceiver.java
+++ src/com/axelby/podax/BluetoothConnectionReceiver.java
@@ -5,18 +5,11 @@ import android.bluetooth.BluetoothDevice;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.preference.PreferenceManager;
 
 // catch all of our Bluetooth events
 public class BluetoothConnectionReceiver extends BroadcastReceiver {
 
 	public void onReceive(Context context, Intent intent) {
-		// If we're not stopping when headphones disconnect, don't worry about it
-		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
-		if (!prefs.getBoolean("stopOnBluetoothPref", true))
-			return;
-
 		// Figure out what kind of device it is
 		BluetoothDevice btDevice = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
 
