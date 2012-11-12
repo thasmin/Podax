3ba9573169b0cdef8229a7c02dbc4cf5782c8ddd
Dan Goldstein <dan@axelby.com>
2012-11-12 10:28:53 -0500

    added menu button to podcast detail activity

--- src/com/axelby/podax/ui/PodcastDetailActivity.java
+++ src/com/axelby/podax/ui/PodcastDetailActivity.java
@@ -1,7 +1,11 @@
 package com.axelby.podax.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuInflater;
+import com.actionbarsherlock.view.MenuItem;
 import com.axelby.podax.R;
 
 public class PodcastDetailActivity extends PodaxFragmentActivity {
@@ -11,4 +15,27 @@ public class PodcastDetailActivity extends PodaxFragmentActivity {
 
         setContentView(R.layout.podcastdetail_activity);
     }
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getSupportMenuInflater();
+		inflater.inflate(R.menu.base, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onMenuItemSelected(int featureId, MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.preferences:
+			startActivity(new Intent(this, Preferences.class));
+			return true;
+		case R.id.changelog:
+			startActivity(new Intent(this, ChangelogActivity.class));
+			return true;
+		case R.id.about:
+			startActivity(new Intent(this, AboutActivity.class));
+			return true;
+		}
+		return super.onMenuItemSelected(featureId, item);
+	}
 }
