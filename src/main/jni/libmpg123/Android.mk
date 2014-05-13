d323b34585b6bfa63455523d6d3adc5586b0e668
Dan Goldstein <dan@axelby.com>
2014-05-13 18:24:51 -0400

    added x86 compilation of ndk audio libraries for genymotion

--- src/main/jni/libmpg123/Android.mk
+++ src/main/jni/libmpg123/Android.mk
@@ -5,6 +5,8 @@ LOCAL_C_INCLUDES := $(LOCAL_PATH)
 LOCAL_MODULE     := mpg123
 LOCAL_ARM_MODE   := arm
 LOCAL_LDLIBS     := -llog
+
+ifeq ($(TARGET_ARCH),arm)
 LOCAL_CFLAGS     := -DACCURATE_ROUNDING \
 					-DOPT_ARM \
 					-DREAL_IS_FIXED \
@@ -14,6 +16,18 @@ LOCAL_CFLAGS     := -DACCURATE_ROUNDING \
 					-DASMALIGN_BYTE \
 					-Wno-int-to-pointer-cast \
 					-Wno-pointer-to-int-cast
+endif
+ifeq ($(TARGET_ARCH),x86)
+LOCAL_CFLAGS     := -DACCURATE_ROUNDING \
+					-DHAVE_STRERROR \
+					-DOPT_SSE \
+					-DREAL_IS_FLOAT \
+					-DNO_REAL \
+					-DNO_32BIT \
+					-DASMALIGN_BYTE \
+					-Wno-int-to-pointer-cast \
+					-Wno-pointer-to-int-cast
+endif
 
 LOCAL_SRC_FILES := 	podax_MPG123.c
 LOCAL_SRC_FILES +=  compat.c
@@ -33,13 +47,27 @@ LOCAL_SRC_FILES +=  parse.c
 LOCAL_SRC_FILES +=  optimize.c
 LOCAL_SRC_FILES +=  synth.c
 LOCAL_SRC_FILES +=  synth_8bit.c
-LOCAL_SRC_FILES +=  synth_arm.S
 LOCAL_SRC_FILES +=  ntom.c
 LOCAL_SRC_FILES +=  dct64.c
 LOCAL_SRC_FILES +=  equalizer.c
-LOCAL_SRC_FILES +=  dither.c
 LOCAL_SRC_FILES +=  tabinit.c
-LOCAL_SRC_FILES +=  synth_arm_accurate.S
 LOCAL_SRC_FILES +=  feature.c
+
+ifeq ($(TARGET_ARCH),arm)
+LOCAL_SRC_FILES +=  synth_arm.S
+LOCAL_SRC_FILES +=  synth_arm_accurate.S
+endif
+ifeq ($(TARGET_ARCH),x86)
+LOCAL_SRC_FILES +=  synth_sse.S
+LOCAL_SRC_FILES +=  synth_sse_accurate.S
+LOCAL_SRC_FILES +=  synth_sse_float.S
+LOCAL_SRC_FILES +=  dct64_sse.S
+LOCAL_SRC_FILES +=  dct64_sse_float.S
+
+LOCAL_SRC_FILES +=  tabinit_mmx.S
+LOCAL_SRC_FILES +=  dct64_i386.c
+LOCAL_SRC_FILES +=  dct36_sse.S
+LOCAL_SRC_FILES +=  synth_stereo_sse_accurate.S
+endif
 
 include $(BUILD_SHARED_LIBRARY)
