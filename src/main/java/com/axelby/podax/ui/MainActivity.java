e3d5cc2f7cba28506eef776cc054d160b097c971
Dan Goldstein <dan@axelby.com>
2014-07-27 16:34:25 -0500

    reindented some files

--- src/main/java/com/axelby/podax/ui/MainActivity.java
+++ src/main/java/com/axelby/podax/ui/MainActivity.java
@@ -73,69 +73,69 @@ public class MainActivity extends Activity {
         }
     }
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-		// check if this was opened by android to save an RSS feed
-		final Intent intent = getIntent();
-		if (intent.getDataString() != null && intent.getData().getScheme().equals("http")) {
-			ContentValues values = new ContentValues();
-			values.put(SubscriptionProvider.COLUMN_URL, intent.getDataString());
-			Uri savedSubscription = getContentResolver().insert(SubscriptionProvider.URI, values);
-			UpdateService.updateSubscription(this, Integer.valueOf(savedSubscription.getLastPathSegment()));
-		}
+        // check if this was opened by android to save an RSS feed
+        final Intent intent = getIntent();
+        if (intent.getDataString() != null && intent.getData().getScheme().equals("http")) {
+            ContentValues values = new ContentValues();
+            values.put(SubscriptionProvider.COLUMN_URL, intent.getDataString());
+            Uri savedSubscription = getContentResolver().insert(SubscriptionProvider.URI, values);
+            UpdateService.updateSubscription(this, Integer.valueOf(savedSubscription.getLastPathSegment()));
+        }
 
-		// clear RSS error notification
-		NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-		notificationManager.cancel(Constants.SUBSCRIPTION_UPDATE_ERROR);
+        // clear RSS error notification
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.cancel(Constants.SUBSCRIPTION_UPDATE_ERROR);
 
-		BootReceiver.setupAlarms(getApplicationContext());
+        BootReceiver.setupAlarms(getApplicationContext());
 
-		if (!isPlayerServiceRunning())
-			PlayerStatus.updateState(this, PlayerStatus.PlayerStates.STOPPED);
+        if (!isPlayerServiceRunning())
+            PlayerStatus.updateState(this, PlayerStatus.PlayerStates.STOPPED);
 
-		// release notes dialog
-		try {
-			PackageManager packageManager = getApplication().getPackageManager();
-			if (packageManager != null) {
-				SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
-				int lastReleaseNoteDialog = preferences.getInt("lastReleaseNoteDialog", 0);
-				int versionCode = packageManager.getPackageInfo(getPackageName(), 0).versionCode;
-				if (lastReleaseNoteDialog < versionCode) {
-					new AlertDialog.Builder(this)
-							.setTitle(R.string.release_notes)
-							.setMessage(R.string.release_notes_detailed)
-							.setPositiveButton(R.string.view_release_notes, new DialogInterface.OnClickListener() {
-								@Override
-								public void onClick(DialogInterface dialogInterface, int i) {
-									//replaceFragment(AboutFragment.class);
-								}
-							})
-							.setNegativeButton(R.string.no_thanks, new DialogInterface.OnClickListener() {
-								@Override
-								public void onClick(DialogInterface dialogInterface, int i) {
-								}
-							})
-							.create()
-							.show();
-					preferences.edit().putInt("lastReleaseNoteDialog", versionCode).apply();
-				}
-			}
-		} catch (PackageManager.NameNotFoundException ignored) {
-		}
+        // release notes dialog
+        try {
+            PackageManager packageManager = getApplication().getPackageManager();
+            if (packageManager != null) {
+                SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+                int lastReleaseNoteDialog = preferences.getInt("lastReleaseNoteDialog", 0);
+                int versionCode = packageManager.getPackageInfo(getPackageName(), 0).versionCode;
+                if (lastReleaseNoteDialog < versionCode) {
+                    new AlertDialog.Builder(this)
+                            .setTitle(R.string.release_notes)
+                            .setMessage(R.string.release_notes_detailed)
+                            .setPositiveButton(R.string.view_release_notes, new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
+                                    //replaceFragment(AboutFragment.class);
+                                }
+                            })
+                            .setNegativeButton(R.string.no_thanks, new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
+                                }
+                            })
+                            .create()
+                            .show();
+                    preferences.edit().putInt("lastReleaseNoteDialog", versionCode).apply();
+                }
+            }
+        } catch (PackageManager.NameNotFoundException ignored) {
+        }
 
-		// ui initialization
-		setContentView(R.layout.app);
+        // ui initialization
+        setContentView(R.layout.app);
 
         ActionBar actionBar = getActionBar();
         if (actionBar != null) {
             actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
 
             actionBar.addTab(actionBar.newTab()
-                    .setText(R.string.playlist)
-                    .setTabListener(new TabListener<PlaylistFragment>(
-                            this, "playlist", PlaylistFragment.class))
+                            .setText(R.string.playlist)
+                            .setTabListener(new TabListener<PlaylistFragment>(
+                                    this, "playlist", PlaylistFragment.class))
             );
 
             actionBar.addTab(actionBar.newTab()
@@ -144,7 +144,7 @@ public class MainActivity extends Activity {
                                     this, "podcasts", SubscriptionListFragment.class))
             );
         }
-	}
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -163,28 +163,28 @@ public class MainActivity extends Activity {
         }
     }
 
-	private void handleGPodder() {
-		AccountManager am = AccountManager.get(this);
-		Account[] gpodder_accounts = am.getAccountsByType(Constants.GPODDER_ACCOUNT_TYPE);
-		if (gpodder_accounts == null || gpodder_accounts.length == 0)
-			startActivity(new Intent(this, AuthenticatorActivity.class));
-		else {
-			Toast.makeText(this, "Refreshing from gpodder.net as " + gpodder_accounts[0].name, Toast.LENGTH_SHORT).show();
-			ContentResolver.requestSync(gpodder_accounts[0], GPodderProvider.AUTHORITY, new Bundle());
-		}
-	}
+    private void handleGPodder() {
+        AccountManager am = AccountManager.get(this);
+        Account[] gpodder_accounts = am.getAccountsByType(Constants.GPODDER_ACCOUNT_TYPE);
+        if (gpodder_accounts == null || gpodder_accounts.length == 0)
+            startActivity(new Intent(this, AuthenticatorActivity.class));
+        else {
+            Toast.makeText(this, "Refreshing from gpodder.net as " + gpodder_accounts[0].name, Toast.LENGTH_SHORT).show();
+            ContentResolver.requestSync(gpodder_accounts[0], GPodderProvider.AUTHORITY, new Bundle());
+        }
+    }
 
-	private boolean isPlayerServiceRunning() {
-		ActivityManager manager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
-		for (ActivityManager.RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE))
-			if (PlayerService.class.getName().equals(service.service.getClassName()))
-				return true;
-		return false;
-	}
+    private boolean isPlayerServiceRunning() {
+        ActivityManager manager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        for (ActivityManager.RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE))
+            if (PlayerService.class.getName().equals(service.service.getClassName()))
+                return true;
+        return false;
+    }
 
-	@Override
-	protected void onResume() {
-		super.onResume();
-		Helper.registerMediaButtons(this);
-	}
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Helper.registerMediaButtons(this);
+    }
 }
