b58fbba8c3dee40c29b8a7b07e8c037b268e779b
Dan Goldstein <dan@axelby.com>
2015-02-08 23:34:41 -0600

    changed latest activity list to have headers instead of dates on each item

--- src/main/java/com/axelby/podax/ui/LatestActivityFragment.java
+++ src/main/java/com/axelby/podax/ui/LatestActivityFragment.java
@@ -15,6 +15,7 @@ import android.text.SpannableString;
 import android.text.Spanned;
 import android.text.method.LinkMovementMethod;
 import android.text.style.ClickableSpan;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -28,11 +29,15 @@ import com.axelby.podax.PlayerService;
 import com.axelby.podax.R;
 import com.axelby.podax.SubscriptionCursor;
 
+import org.joda.time.DateTime;
 import org.joda.time.Instant;
 import org.joda.time.Period;
 import org.joda.time.format.PeriodFormat;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 import javax.annotation.Nonnull;
 
@@ -74,7 +79,24 @@ public class LatestActivityFragment extends Fragment implements LoaderManager.Lo
 	}
 
 	public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
-		_adapter.changeCursor(cursor);
+		// could really use algebraic data types here
+		List<Object> items = new ArrayList<>(cursor.getCount() * 2);
+		String lastPeriod = null;
+		while (cursor.moveToNext()) {
+			Date date = new EpisodeCursor(cursor).getPubDate();
+			Period timeDiff = new Period(new DateTime(date.getTime()), Instant.now());
+
+			String period = PeriodFormat.wordBased().print(timeDiff);
+			if (period.indexOf(',') > 0)
+				period = period.substring(0, period.indexOf(','));
+			period = period + " ago";
+
+			if (!period.equals(lastPeriod))
+				items.add(period);
+			items.add(cursor.getPosition());
+			lastPeriod = period;
+		}
+		_adapter.changeItems(items, cursor);
 		_adapter.notifyDataSetChanged();
 	}
 
@@ -82,20 +104,32 @@ public class LatestActivityFragment extends Fragment implements LoaderManager.Lo
 		if (getActivity() == null)
 			return;
 
-		_adapter.changeCursor(null);
+		_adapter.clear();
 		_adapter.notifyDataSetChanged();
 	}
 
-	private class LatestActivityAdapter extends RecyclerView.Adapter<LatestActivityAdapter.ViewHolder> {
-		private Cursor _cursor;
+	private class LatestActivityAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+		private Cursor _cursor = null;
+		private List<Object> _items = null;
 
-		public class ViewHolder extends RecyclerView.ViewHolder {
+		private final int TYPE_HEADER = 0;
+		private final int TYPE_ACTIVITY = 1;
+
+		public class HeaderHolder extends RecyclerView.ViewHolder {
+			public TextView header;
+			public HeaderHolder(View view) {
+				super(view);
+				header = (TextView) view;
+			}
+		}
+
+		public class ActivityHolder extends RecyclerView.ViewHolder {
 			public ImageView subscription_img;
 			public TextView description;
 			public TextView downloaded;
 			public View play;
 
-			public ViewHolder(View view) {
+			public ActivityHolder(View view) {
 				super(view);
 
 				subscription_img = (ImageView) view.findViewById(R.id.subscription_img);
@@ -117,14 +151,45 @@ public class LatestActivityFragment extends Fragment implements LoaderManager.Lo
 			setHasStableIds(true);
 		}
 
-		public void changeCursor(Cursor cursor) {
+		public void changeItems(List<Object> items, Cursor cursor) {
+			_items = items;
 			_cursor = cursor;
 		}
 
+		public void clear() { _cursor = null; }
+
 		@Override
-		public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-			View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item_latest_activity, parent, false);
-			return new ViewHolder(view);
+		public long getItemId(int position) {
+			if (_items.get(position) instanceof String) {
+				return _items.get(position).hashCode();
+			}
+			_cursor.moveToPosition((Integer) _items.get(position));
+			return new EpisodeCursor(_cursor).getId();
+		}
+
+		@Override
+		public int getItemCount() {
+			if (_cursor == null)
+				return 0;
+			return _items.size();
+		}
+
+		@Override
+		public int getItemViewType(int position) {
+			return _items.get(position) instanceof String ? TYPE_HEADER : TYPE_ACTIVITY;
+		}
+
+		@Override
+		public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+			if (viewType == TYPE_ACTIVITY) {
+				View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item_latest_activity, parent, false);
+				return new ActivityHolder(view);
+			} else {
+				TextView tv = new TextView(parent.getContext());
+				tv.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+				tv.setTextSize(TypedValue.COMPLEX_UNIT_SP, 24);
+				return new HeaderHolder(tv);
+			}
 		}
 
 		public class IntentSpan extends ClickableSpan {
@@ -134,60 +199,51 @@ public class LatestActivityFragment extends Fragment implements LoaderManager.Lo
 		}
 
 		@Override
-		public void onBindViewHolder(ViewHolder holder, int position) {
+		public void onBindViewHolder(RecyclerView.ViewHolder viewHolder, int position) {
 			if (_cursor == null)
 				return;
-			_cursor.moveToPosition(position);
-			EpisodeCursor episode = new EpisodeCursor(_cursor);
 
-			holder.play.setTag(episode.getId());
-			holder.subscription_img.setImageBitmap(SubscriptionCursor.getThumbnailImage(getActivity(), episode.getSubscriptionId()));
+			int viewType = getItemViewType(position);
+			if (viewType == TYPE_ACTIVITY) {
+				ActivityHolder holder = (ActivityHolder) viewHolder;
 
-			// get data for description and spannables
-			Period diff = new Period(episode.getPubDate().getTime(), Instant.now().getMillis());
-			String timeAgo = PeriodFormat.wordBased().print(diff);
-			if (timeAgo.indexOf(',') > 0)
-				timeAgo = timeAgo.substring(0, timeAgo.indexOf(','));
-			timeAgo = timeAgo + " ago, ";
+				_cursor.moveToPosition((Integer) _items.get(position));
+				EpisodeCursor episode = new EpisodeCursor(_cursor);
 
-			String subTitle = episode.getSubscriptionTitle();
-			Intent subIntent = PodaxFragmentActivity.createIntent(getActivity(), EpisodeListFragment.class, Constants.EXTRA_SUBSCRIPTION_ID, episode.getSubscriptionId());
-			int subSpanStart = timeAgo.length();
+				holder.play.setTag(episode.getId());
+				holder.subscription_img.setImageBitmap(SubscriptionCursor.getThumbnailImage(getActivity(), episode.getSubscriptionId()));
 
-			String epTitle = episode.getTitle();
-			Intent epIntent = PodaxFragmentActivity.createIntent(getActivity(), EpisodeDetailFragment.class, Constants.EXTRA_EPISODE_ID, episode.getId());
-			int epSpanStart = subSpanStart + subTitle.length() + " released ".length();
+				// get data for description and spannables
+				String subTitle = episode.getSubscriptionTitle();
+				Intent subIntent = PodaxFragmentActivity.createIntent(getActivity(), EpisodeListFragment.class, Constants.EXTRA_SUBSCRIPTION_ID, episode.getSubscriptionId());
+				int subSpanStart = 0;
 
-			String spanText = String.format("%s%s released %s", timeAgo, subTitle, epTitle);
+				String epTitle = episode.getTitle();
+				Intent epIntent = PodaxFragmentActivity.createIntent(getActivity(), EpisodeDetailFragment.class, Constants.EXTRA_EPISODE_ID, episode.getId());
+				int epSpanStart = subSpanStart + subTitle.length() + " released ".length();
 
-			// set description and spannables
-			SpannableString description = new SpannableString(spanText);
-			description.setSpan(new IntentSpan(subIntent), subSpanStart, subSpanStart + subTitle.length(), Spanned.SPAN_MARK_MARK);
-			description.setSpan(new IntentSpan(epIntent), epSpanStart, spanText.length(), Spanned.SPAN_MARK_MARK);
-			holder.description.setText(description);
-			holder.description.setMovementMethod(LinkMovementMethod.getInstance());
+				String spanText = String.format("%s released %s", subTitle, epTitle);
 
-            float downloaded = new File(episode.getFilename(getActivity())).length();
-            if (episode.getFileSize() != downloaded) {
-                holder.downloaded.setTextColor(0xffcc0000); //android.R.color.holo_red_dark
-                holder.downloaded.setText(R.string.not_downloaded);
-            } else {
-                holder.downloaded.setTextColor(0xff669900); //android.R.color.holo_green_dark
-                holder.downloaded.setText(R.string.downloaded);
-            }
-		}
+				// set description and spannables
+				SpannableString description = new SpannableString(spanText);
+				description.setSpan(new IntentSpan(subIntent), subSpanStart, subSpanStart + subTitle.length(), Spanned.SPAN_MARK_MARK);
+				description.setSpan(new IntentSpan(epIntent), epSpanStart, spanText.length(), Spanned.SPAN_MARK_MARK);
+				holder.description.setText(description);
+				holder.description.setMovementMethod(LinkMovementMethod.getInstance());
 
-		@Override
-		public long getItemId(int position) {
-			_cursor.moveToPosition(position);
-			return new EpisodeCursor(_cursor).getId();
-		}
-
-		@Override
-		public int getItemCount() {
-			if (_cursor == null)
-				return 0;
-			return _cursor.getCount();
+				float downloaded = new File(episode.getFilename(getActivity())).length();
+				if (episode.getFileSize() != downloaded) {
+					holder.downloaded.setTextColor(0xffcc0000); //android.R.color.holo_red_dark
+					holder.downloaded.setText(R.string.not_downloaded);
+				} else {
+					holder.downloaded.setTextColor(0xff669900); //android.R.color.holo_green_dark
+					holder.downloaded.setText(R.string.downloaded);
+				}
+			} else if (viewType == TYPE_HEADER) {
+				HeaderHolder holder = (HeaderHolder) viewHolder;
+				String period = (String) _items.get(position);
+				holder.header.setText(period);
+			}
 		}
 	}
 }
