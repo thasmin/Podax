3d58340d75a9ae14bf759b7bd5ebd92c3f47bfca
Dan Goldstein <dan@axelby.com>
2015-03-25 22:08:26 -0500

    switch search to fragment to allow for other search sources

--- src/main/java/com/axelby/podax/ui/SearchActivity.java
+++ src/main/java/com/axelby/podax/ui/SearchActivity.java
@@ -1,62 +1,63 @@
 package com.axelby.podax.ui;
 
-import android.app.LoaderManager;
+import android.app.Fragment;
+import android.app.FragmentManager;
 import android.app.SearchManager;
-import android.content.CursorLoader;
 import android.content.Intent;
-import android.content.Loader;
-import android.database.Cursor;
-import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.support.v13.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.view.LayoutInflater;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
 import android.widget.EditText;
-import android.widget.GridLayout;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.TextView;
 
-import com.axelby.podax.Constants;
-import com.axelby.podax.EpisodeCursor;
-import com.axelby.podax.EpisodeProvider;
 import com.axelby.podax.Helper;
 import com.axelby.podax.R;
-import com.axelby.podax.SubscriptionCursor;
-import com.axelby.podax.SubscriptionProvider;
 
-import java.util.HashMap;
+public class SearchActivity extends ActionBarActivity {
+	public interface QueryChangedHandler {
+		public void onQueryChanged(String query);
+	}
+	private QueryChangedHandler[] _fragments = new QueryChangedHandler[3];
 
-public class SearchActivity extends ActionBarActivity implements LoaderManager.LoaderCallbacks<Cursor> {
-	private GridLayout _subscriptionList;
-	private TextView _subscriptionEmpty;
-
-	private LinearLayout _episodeList;
-	private TextView _episodeEmpty;
-
-	private HashMap<Long, Bitmap> _thumbnails = new HashMap<>();
-
-	private View.OnClickListener _subscriptionClickHandler = new View.OnClickListener() {
-		@Override
-		public void onClick(View view) {
-			long id = (long) view.getTag();
-			startActivity(PodaxFragmentActivity.createIntent(view.getContext(), EpisodeListFragment.class, Constants.EXTRA_SUBSCRIPTION_ID, id));
+	public class SearchAdapter extends FragmentPagerAdapter {
+		public SearchAdapter(FragmentManager fragmentManager) {
+			super(fragmentManager);
 		}
-	};
 
-	private final View.OnClickListener _episodeClickHandler = new View.OnClickListener() {
 		@Override
-		public void onClick(View view) {
-			long id = (long) view.getTag();
-			startActivity(PodaxFragmentActivity.createIntent(view.getContext(), EpisodeDetailFragment.class, Constants.EXTRA_EPISODE_ID, id));
+		public int getCount() {
+			return 3;
 		}
-	};
+
+		@Override
+		public Fragment getItem(int position) {
+			if (_fragments[position] == null) {
+				switch (position) {
+					case 0: _fragments[position] = new SearchPodaxFragment(); break;
+					case 1: return new Fragment();
+					case 2: return new Fragment();
+					default: return null;
+				}
+			}
+
+			return (Fragment) _fragments[position];
+		}
+
+		@Override
+		public CharSequence getPageTitle(int position) {
+			switch (position) {
+				case 0: return "Subscriptions";
+				case 1: return "podaxapp.com";
+				case 2: return "gpodder.net";
+				default: return "";
+			}
+		}
+	}
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -67,12 +68,6 @@ public class SearchActivity extends ActionBarActivity implements LoaderManager.L
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null)
 		    actionBar.setDisplayHomeAsUpEnabled(true);
-
-		_subscriptionList = (GridLayout) findViewById(R.id.subscription_list);
-		_subscriptionEmpty = (TextView) findViewById(R.id.subscription_empty);
-
-		_episodeList = (LinearLayout) findViewById(R.id.episode_list);
-		_episodeEmpty = (TextView) findViewById(R.id.episode_empty);
 
 		// Get the intent, verify the action and get the query
 		Intent intent = getIntent();
@@ -95,15 +90,14 @@ public class SearchActivity extends ActionBarActivity implements LoaderManager.L
 
 			@Override
 			public void onTextChanged(CharSequence str, int start, int before, int count) {
-				Bundle args = new Bundle(1);
-				args.putString(SearchManager.QUERY, str.toString());
-				getLoaderManager().restartLoader(0, args, SearchActivity.this);
-				getLoaderManager().restartLoader(1, args, SearchActivity.this);
+				for (QueryChangedHandler fragment : _fragments)
+					if (fragment != null)
+						fragment.onQueryChanged(str.toString());
 			}
 		});
 
-		getLoaderManager().initLoader(0, intent.getExtras(), this);
-		getLoaderManager().initLoader(1, intent.getExtras(), this);
+		ViewPager pager = (ViewPager) findViewById(R.id.pager);
+		pager.setAdapter(new SearchAdapter(getFragmentManager()));
 	}
 
 	@Override
@@ -121,90 +115,5 @@ public class SearchActivity extends ActionBarActivity implements LoaderManager.L
 	protected void onResume() {
 		super.onResume();
 		Helper.registerMediaButtons(this);
-	}
-
-	@Override
-	public Loader<Cursor> onCreateLoader(int loader, Bundle bundle) {
-		String query = bundle.getString(SearchManager.QUERY);
-		if (loader == 0)
-			return new CursorLoader(this, SubscriptionProvider.SEARCH_URI, null, null, new String[] { query }, null);
-		else if (loader == 1)
-			return new CursorLoader(this, EpisodeProvider.SEARCH_URI, null, null, new String[] { query }, null);
-		return null;
-	}
-
-	private Bitmap getThumbnail(long subId) {
-		if (_thumbnails.containsKey(subId))
-			return _thumbnails.get(subId);
-		Bitmap thumb = SubscriptionCursor.getThumbnailImage(this, subId);
-		_thumbnails.put(subId, thumb);
-		return thumb;
-	}
-
-	@Override
-	public void onLoadFinished(Loader<Cursor> cursorLoader, Cursor cursor) {
-		LayoutInflater inflater = LayoutInflater.from(this);
-		if (cursorLoader.getId() == 0) {
-			boolean isEmpty = cursor.getCount() == 0;
-			_subscriptionList.setVisibility(isEmpty ? View.GONE : View.VISIBLE);
-			_subscriptionEmpty.setVisibility(isEmpty ? View.VISIBLE : View.GONE);
-
-			_subscriptionList.removeAllViews();
-			int rowCount = (int) Math.ceil(cursor.getCount() / 3.0f);
-			_subscriptionList.setRowCount(rowCount);
-
-			int thumbSize = _subscriptionList.getMeasuredWidth() / 3;
-			thumbSize -= getResources().getDisplayMetrics().density * 10;
-
-			while (cursor.moveToNext()) {
-				SubscriptionCursor sub = new SubscriptionCursor(cursor);
-				View view = inflater.inflate(R.layout.search_item_subscription, _subscriptionList, false);
-				view.setOnClickListener(_subscriptionClickHandler);
-				view.setTag(sub.getId());
-
-				ImageView thumbnail = (ImageView) view.findViewById(R.id.thumbnail);
-				thumbnail.setLayoutParams(new LinearLayout.LayoutParams(thumbSize, thumbSize));
-				thumbnail.setImageBitmap(getThumbnail(sub.getId()));
-
-				TextView title = (TextView) view.findViewById(R.id.title);
-				title.setLayoutParams(new LinearLayout.LayoutParams(thumbSize, ViewGroup.LayoutParams.WRAP_CONTENT));
-				title.setText(sub.getTitle());
-
-				_subscriptionList.addView(view);
-			}
-		}
-		else if (cursorLoader.getId() == 1) {
-			boolean isEmpty = cursor.getCount() == 0;
-			_episodeList.setVisibility(isEmpty ? View.GONE : View.VISIBLE);
-			_episodeEmpty.setVisibility(isEmpty ? View.VISIBLE : View.GONE);
-
-			while (cursor.moveToNext()) {
-				EpisodeCursor ep = new EpisodeCursor(cursor);
-				View view = inflater.inflate(R.layout.search_item_episode, _episodeList, false);
-				view.setOnClickListener(_episodeClickHandler);
-				view.setTag(ep.getId());
-
-				ImageView thumbnail = (ImageView) view.findViewById(R.id.thumbnail);
-				thumbnail.setImageBitmap(SubscriptionCursor.getThumbnailImage(SearchActivity.this, ep.getSubscriptionId()));
-
-				TextView title = (TextView) view.findViewById(R.id.title);
-				title.setText(ep.getTitle());
-
-				_episodeList.addView(view);
-			}
-		}
-	}
-
-	@Override
-	public void onLoaderReset(Loader<Cursor> cursorLoader) {
-		if (cursorLoader.getId() == 0) {
-			_subscriptionList.setVisibility(View.GONE);
-			_subscriptionList.removeAllViews();
-			_subscriptionEmpty.setVisibility(View.VISIBLE);
-		} else if (cursorLoader.getId() == 1) {
-			_episodeList.setVisibility(View.GONE);
-			_episodeList.removeAllViews();
-			_episodeEmpty.setVisibility(View.VISIBLE);
-		}
 	}
 }
---
+++ src/main/java/com/axelby/podax/ui/SearchPodaxFragment.java
@@ -0,0 +1,171 @@
+package com.axelby.podax.ui;
+
+import android.app.Fragment;
+import android.app.LoaderManager;
+import android.app.SearchManager;
+import android.content.Context;
+import android.content.CursorLoader;
+import android.content.Loader;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.GridLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.axelby.podax.Constants;
+import com.axelby.podax.EpisodeCursor;
+import com.axelby.podax.EpisodeProvider;
+import com.axelby.podax.R;
+import com.axelby.podax.SubscriptionCursor;
+import com.axelby.podax.SubscriptionProvider;
+
+import java.util.HashMap;
+
+public class SearchPodaxFragment extends Fragment
+		implements SearchActivity.QueryChangedHandler, LoaderManager.LoaderCallbacks<Cursor> {
+	private GridLayout _subscriptionList;
+	private TextView _subscriptionEmpty;
+
+	private LinearLayout _episodeList;
+	private TextView _episodeEmpty;
+
+	private HashMap<Long, Bitmap> _thumbnails = new HashMap<>();
+
+	private View.OnClickListener _subscriptionClickHandler = new View.OnClickListener() {
+		@Override
+		public void onClick(View view) {
+			long id = (long) view.getTag();
+			startActivity(PodaxFragmentActivity.createIntent(view.getContext(), EpisodeListFragment.class, Constants.EXTRA_SUBSCRIPTION_ID, id));
+		}
+	};
+
+	private final View.OnClickListener _episodeClickHandler = new View.OnClickListener() {
+		@Override
+		public void onClick(View view) {
+			long id = (long) view.getTag();
+			startActivity(PodaxFragmentActivity.createIntent(view.getContext(), EpisodeDetailFragment.class, Constants.EXTRA_EPISODE_ID, id));
+		}
+	};
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		return inflater.inflate(R.layout.search_podax_fragment, container, false);
+	}
+
+	@Override
+	public void onViewCreated(View view, Bundle savedInstanceState) {
+		_subscriptionList = (GridLayout) view.findViewById(R.id.subscription_list);
+		_subscriptionEmpty = (TextView) view.findViewById(R.id.subscription_empty);
+
+		_episodeList = (LinearLayout) view.findViewById(R.id.episode_list);
+		_episodeEmpty = (TextView) view.findViewById(R.id.episode_empty);
+
+	}
+
+	@Override
+	public void onQueryChanged(String query) {
+		Bundle args = new Bundle(1);
+		args.putString(SearchManager.QUERY, query);
+		getLoaderManager().restartLoader(0, args, this);
+		getLoaderManager().restartLoader(1, args, this);
+	}
+
+	private Bitmap getThumbnail(long subId) {
+		if (_thumbnails.containsKey(subId))
+			return _thumbnails.get(subId);
+		Bitmap thumb = SubscriptionCursor.getThumbnailImage(getActivity(), subId);
+		_thumbnails.put(subId, thumb);
+		return thumb;
+	}
+
+	@Override
+	public Loader<Cursor> onCreateLoader(int loader, Bundle bundle) {
+		String query = bundle.getString(SearchManager.QUERY);
+		if (loader == 0)
+			return new CursorLoader(getActivity(), SubscriptionProvider.SEARCH_URI, null, null, new String[] { query }, null);
+		else if (loader == 1)
+			return new CursorLoader(getActivity(), EpisodeProvider.SEARCH_URI, null, null, new String[] { query }, null);
+		return null;
+	}
+
+	@Override
+	public void onLoadFinished(Loader<Cursor> cursorLoader, Cursor cursor) {
+		if (getActivity() == null)
+			return;
+
+		Context context = getActivity();
+		LayoutInflater inflater = LayoutInflater.from(context);
+		if (cursorLoader.getId() == 0) {
+			boolean isEmpty = cursor.getCount() == 0;
+			_subscriptionList.setVisibility(isEmpty ? View.GONE : View.VISIBLE);
+			_subscriptionEmpty.setVisibility(isEmpty ? View.VISIBLE : View.GONE);
+
+			_subscriptionList.removeAllViews();
+			int rowCount = (int) Math.ceil(cursor.getCount() / 3.0f);
+			_subscriptionList.setRowCount(rowCount);
+
+			int thumbSize = _subscriptionList.getMeasuredWidth() / 3;
+			thumbSize -= getResources().getDisplayMetrics().density * 10;
+
+			while (cursor.moveToNext()) {
+				SubscriptionCursor sub = new SubscriptionCursor(cursor);
+				View view = inflater.inflate(R.layout.search_item_subscription, _subscriptionList, false);
+				view.setOnClickListener(_subscriptionClickHandler);
+				view.setTag(sub.getId());
+
+				ImageView thumbnail = (ImageView) view.findViewById(R.id.thumbnail);
+				thumbnail.setLayoutParams(new LinearLayout.LayoutParams(thumbSize, thumbSize));
+				thumbnail.setImageBitmap(getThumbnail(sub.getId()));
+
+				TextView title = (TextView) view.findViewById(R.id.title);
+				title.setLayoutParams(new LinearLayout.LayoutParams(thumbSize, ViewGroup.LayoutParams.WRAP_CONTENT));
+				title.setText(sub.getTitle());
+
+				_subscriptionList.addView(view);
+			}
+		}
+		else if (cursorLoader.getId() == 1) {
+			boolean isEmpty = cursor.getCount() == 0;
+			_episodeList.setVisibility(isEmpty ? View.GONE : View.VISIBLE);
+			_episodeEmpty.setVisibility(isEmpty ? View.VISIBLE : View.GONE);
+
+			while (cursor.moveToNext()) {
+				EpisodeCursor ep = new EpisodeCursor(cursor);
+				View view = inflater.inflate(R.layout.search_item_episode, _episodeList, false);
+				view.setOnClickListener(_episodeClickHandler);
+				view.setTag(ep.getId());
+
+				ImageView thumbnail = (ImageView) view.findViewById(R.id.thumbnail);
+				thumbnail.setImageBitmap(SubscriptionCursor.getThumbnailImage(context, ep.getSubscriptionId()));
+
+				TextView title = (TextView) view.findViewById(R.id.title);
+				title.setText(ep.getTitle());
+
+				_episodeList.addView(view);
+			}
+		}
+	}
+
+	@Override
+	public void onLoaderReset(Loader<Cursor> cursorLoader) {
+		if (cursorLoader.getId() == 0) {
+			_subscriptionList.setVisibility(View.GONE);
+			_subscriptionList.removeAllViews();
+			_subscriptionEmpty.setVisibility(View.VISIBLE);
+		} else if (cursorLoader.getId() == 1) {
+			_episodeList.setVisibility(View.GONE);
+			_episodeList.removeAllViews();
+			_episodeEmpty.setVisibility(View.VISIBLE);
+		}
+	}
+}
