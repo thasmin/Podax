0c1dd0b6b9e52d10e4503cef8bbbfeb76a87ff23
Dan Goldstein <dan@axelby.com>
2015-03-30 21:26:32 -0500

    changed subscription detail fragment layout

--- src/main/java/com/axelby/podax/ui/EpisodeListFragment.java
+++ src/main/java/com/axelby/podax/ui/EpisodeListFragment.java
@@ -11,10 +11,13 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.Loader;
 import android.database.Cursor;
+import android.graphics.Bitmap;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.content.LocalBroadcastManager;
+import android.support.v7.graphics.Palette;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -22,6 +25,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -42,80 +46,94 @@ import javax.annotation.Nonnull;
 
 public class EpisodeListFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor> {
 	private PodcastAdapter _adapter = null;
-	private RecyclerView _listView;
 	private long _subscriptionId;
+
 	private View _currentlyUpdatingMessage;
+	private View _top;
+	private RecyclerView _listView;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
+		setHasOptionsMenu(true);
+
+		// extract subscription id
 		if (getArguments() == null)
 			throw new IllegalArgumentException("EpisodeListFragment needs a subscription id argument");
 		_subscriptionId = getArguments().getLong(Constants.EXTRA_SUBSCRIPTION_ID, -1);
 		if (_subscriptionId == -1)
 			throw new IllegalArgumentException("EpisodeListFragment needs a subscription id argument");
 
-		setHasOptionsMenu(true);
-
-		_adapter = new PodcastAdapter();
 		getLoaderManager().initLoader(0, null, this);
 	}
 
 	@Override
 	public View onCreateView(@Nonnull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-		return inflater.inflate(R.layout.fragment_episodelist, container, false);
+		return inflater.inflate(R.layout.episodelist_fragment, container, false);
 	}
 
 	@Override
 	public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
 		super.onViewCreated(view, savedInstanceState);
 
-		_listView = (RecyclerView) getActivity().findViewById(R.id.list);
-		_listView.setLayoutManager(new LinearLayoutManager(getActivity()));
+		Context context = view.getContext();
+
+		_top = view.findViewById(R.id.top);
+		ViewTreeObserver.OnGlobalLayoutListener layoutListener = new ViewTreeObserver.OnGlobalLayoutListener() {
+			@Override
+			public void onGlobalLayout() {
+				_listView.setPadding(0, _top.getHeight(), 0, 0);
+				ViewTreeObserver vto = _top.getViewTreeObserver();
+				if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+					vto.removeOnGlobalLayoutListener(this);
+				} else {
+					vto.removeGlobalOnLayoutListener(this);
+				}
+			}
+		};
+		_top.getViewTreeObserver().addOnGlobalLayoutListener(layoutListener);
+
+		_listView = (RecyclerView) view.findViewById(R.id.list);
+		_listView.setLayoutManager(new LinearLayoutManager(context));
 		_listView.setItemAnimator(new DefaultItemAnimator());
+		_listView.setOnScrollListener(new RecyclerView.OnScrollListener() {
+			@Override
+			public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+				super.onScrolled(recyclerView, dx, dy);
+				_top.setTranslationY(_top.getTranslationY() - dy);
+			}
+		});
 
-		_currentlyUpdatingMessage = view.findViewById(R.id.currently_updating);
-	}
-
-	@Override
-	public void onActivityCreated(Bundle savedInstanceState) {
-		super.onActivityCreated(savedInstanceState);
-
+		_adapter = new PodcastAdapter();
 		_listView.setAdapter(_adapter);
 
-        setTitle();
+		_currentlyUpdatingMessage = view.findViewById(R.id.currently_updating);
+
+		setupHeader();
 	}
 
-	void setTitle() {
-		if (getView() == null)
+	void setupHeader() {
+		View view = getView();
+		if (view == null)
 			return;
 
-        long subscriptionId = getArguments().getLong(Constants.EXTRA_SUBSCRIPTION_ID, -1);
-        if (subscriptionId == -1)
-            return;
-
-		Uri subscriptionUri = ContentUris.withAppendedId(SubscriptionProvider.URI, subscriptionId);
-		String[] subscriptionProjection = {
-				SubscriptionProvider.COLUMN_ID,
-				SubscriptionProvider.COLUMN_TITLE,
-				SubscriptionProvider.COLUMN_URL,
-		};
-		Cursor subscriptionCursor = getActivity().getContentResolver().query(subscriptionUri, subscriptionProjection, null, null, null);
+		Context context = view.getContext();
+		SubscriptionCursor subscriptionCursor = SubscriptionCursor.getCursor(context, _subscriptionId);
 		if (subscriptionCursor == null)
 			return;
 
-        if (subscriptionCursor.moveToNext()) {
-            SubscriptionCursor cursor = new SubscriptionCursor(subscriptionCursor);
+		ImageView thumbnail = (ImageView) view.findViewById(R.id.thumbnail);
+		Bitmap thumbnailImage = SubscriptionCursor.getThumbnailImage(context, _subscriptionId);
+		thumbnail.setImageBitmap(thumbnailImage);
 
-            TextView subscription = (TextView) getView().findViewById(R.id.subscription);
-            subscription.setText(cursor.getTitle());
+		Palette palette = Palette.generate(thumbnailImage);
+		TextView title = (TextView) view.findViewById(R.id.subscription);
+		title.setText(subscriptionCursor.getTitle());
+		title.setBackgroundColor(palette.getMutedColor(R.color.background_material_dark));
+		title.setTextColor(palette.getVibrantColor(R.color.primary_text_default_material_dark));
 
-            ImageView thumbnail = (ImageView) getView().findViewById(R.id.thumbnail);
-            thumbnail.setImageBitmap(SubscriptionCursor.getThumbnailImage(getActivity(), subscriptionId));
-        }
-
-        subscriptionCursor.close();
+		subscriptionCursor.closeCursor();
 	}
 
 	private BroadcastReceiver _updateReceiver = new BroadcastReceiver() {
@@ -126,7 +144,7 @@ public class EpisodeListFragment extends Fragment implements LoaderManager.Loade
 				_currentlyUpdatingMessage.setVisibility(View.VISIBLE);
 			} else {
 				if (_currentlyUpdatingMessage.getVisibility() == View.VISIBLE)
-					setTitle();
+					setupHeader();
 				_currentlyUpdatingMessage.setVisibility(View.GONE);
 			}
 		}
@@ -135,29 +153,38 @@ public class EpisodeListFragment extends Fragment implements LoaderManager.Loade
 	@Override
 	public void onPause() {
 		super.onPause();
-		LocalBroadcastManager.getInstance(getActivity()).unregisterReceiver(_updateReceiver);
+
+		if (getView() == null)
+			return;
+		LocalBroadcastManager.getInstance(getView().getContext()).unregisterReceiver(_updateReceiver);
 	}
 
 	@Override
 	public void onResume() {
 		super.onResume();
-		IntentFilter intentFilter = new IntentFilter(Constants.ACTION_UPDATE_SUBSCRIPTION);
-		intentFilter.addDataScheme("content");
-		LocalBroadcastManager.getInstance(getActivity()).registerReceiver(_updateReceiver, intentFilter);
 
 		boolean isCurrentlyUpdating = (UpdateService.getUpdatingSubscriptionId() == _subscriptionId);
 		_currentlyUpdatingMessage.setVisibility(isCurrentlyUpdating ? View.VISIBLE : View.GONE);
+
+		if (getView() == null)
+			return;
+		IntentFilter intentFilter = new IntentFilter(Constants.ACTION_UPDATE_SUBSCRIPTION);
+		intentFilter.addDataScheme("content");
+		LocalBroadcastManager.getInstance(getView().getContext()).registerReceiver(_updateReceiver, intentFilter);
 	}
 
 	@Override
 	public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+		if (getActivity() == null)
+			return null;
+
 		Uri uri = ContentUris.withAppendedId(SubscriptionProvider.URI, _subscriptionId);
 		uri = Uri.withAppendedPath(uri, "podcasts");
 		String[] projection = {
 				EpisodeProvider.COLUMN_ID,
 				EpisodeProvider.COLUMN_TITLE,
-                EpisodeProvider.COLUMN_PUB_DATE,
-                EpisodeProvider.COLUMN_DURATION,
+				EpisodeProvider.COLUMN_PUB_DATE,
+				EpisodeProvider.COLUMN_DURATION,
 				EpisodeProvider.COLUMN_MEDIA_URL,
 				EpisodeProvider.COLUMN_FILE_SIZE,
 				EpisodeProvider.COLUMN_PLAYLIST_POSITION,
@@ -167,11 +194,8 @@ public class EpisodeListFragment extends Fragment implements LoaderManager.Loade
 
 	@Override
 	public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
-		if (getActivity() == null)
-			return;
-
 		_adapter.changeCursor(cursor);
-		setTitle();
+		setupHeader();
 	}
 
 	@Override
@@ -181,47 +205,47 @@ public class EpisodeListFragment extends Fragment implements LoaderManager.Loade
 
 	private class PodcastAdapter extends RecyclerView.Adapter<PodcastAdapter.ViewHolder> {
 
-        private final DateFormat _pubDateFormat = DateFormat.getDateInstance();
+		private final DateFormat _pubDateFormat = DateFormat.getDateInstance();
 		private Cursor _cursor;
 
-        class ViewHolder extends RecyclerView.ViewHolder {
+		class ViewHolder extends RecyclerView.ViewHolder {
 			public final View container;
-            public final TextView title;
-            public final TextView date;
-            public final TextView duration;
-            public final Button play;
-            public final Button playlist;
+			public final TextView title;
+			public final TextView date;
+			public final TextView duration;
+			public final Button play;
+			public final Button playlist;
 
-            public ViewHolder (View view) {
+			public ViewHolder (View view) {
 				super(view);
 
 				container = view;
-                title = (TextView) view.findViewById(R.id.title);
-                date = (TextView) view.findViewById(R.id.date);
-                duration = (TextView) view.findViewById(R.id.duration);
-                play = (Button) view.findViewById(R.id.play);
-                playlist = (Button) view.findViewById(R.id.playlist);
+				title = (TextView) view.findViewById(R.id.title);
+				date = (TextView) view.findViewById(R.id.date);
+				duration = (TextView) view.findViewById(R.id.duration);
+				play = (Button) view.findViewById(R.id.play);
+				playlist = (Button) view.findViewById(R.id.playlist);
 
 				play.setOnClickListener(_playHandler);
 				playlist.setOnClickListener(_playlistHandler);
 				view.setOnClickListener(_clickHandler);
-            }
-        }
+			}
+		}
 
 		public PodcastAdapter() {
 			setHasStableIds(true);
-        }
+		}
 
 		public void changeCursor(Cursor cursor) {
 			_cursor = cursor;
 			notifyDataSetChanged();
 		}
 
-        final OnClickListener _playHandler = new OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                long episodeId = (Long) view.getTag();
-                PlayerService.play(view.getContext(), episodeId);
+		final OnClickListener _playHandler = new OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				long episodeId = (Long) view.getTag();
+				PlayerService.play(view.getContext(), episodeId);
 
 				// put podcast on top of playlist
 				ContentValues values = new ContentValues(1);
@@ -229,20 +253,20 @@ public class EpisodeListFragment extends Fragment implements LoaderManager.Loade
 				view.getContext().getContentResolver().update(EpisodeProvider.getContentUri(episodeId), values, null, null);
 
 				startActivity(PodaxFragmentActivity.createIntent(getActivity(), EpisodeDetailFragment.class, Constants.EXTRA_EPISODE_ID, episodeId));
-            }
-        };
+			}
+		};
 
-        final OnClickListener _playlistHandler = new OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                long episodeId = (Long) view.getTag(R.id.episodeId);
-                Integer position = (Integer) view.getTag(R.id.playlist);
+		final OnClickListener _playlistHandler = new OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				long episodeId = (Long) view.getTag(R.id.episodeId);
+				Integer position = (Integer) view.getTag(R.id.playlist);
 
-                ContentValues values = new ContentValues(1);
-                values.put(EpisodeProvider.COLUMN_PLAYLIST_POSITION, position);
-                view.getContext().getContentResolver().update(EpisodeProvider.getContentUri(episodeId), values, null, null);
-            }
-        };
+				ContentValues values = new ContentValues(1);
+				values.put(EpisodeProvider.COLUMN_PLAYLIST_POSITION, position);
+				view.getContext().getContentResolver().update(EpisodeProvider.getContentUri(episodeId), values, null, null);
+			}
+		};
 
 		final OnClickListener _clickHandler = new OnClickListener() {
 			@Override
@@ -254,7 +278,7 @@ public class EpisodeListFragment extends Fragment implements LoaderManager.Loade
 
 		@Override
 		public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-			View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.fragment_episoidelist_item, parent, false);
+			View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.episodelist_item, parent, false);
 			return new ViewHolder(view);
 		}
 
@@ -268,24 +292,24 @@ public class EpisodeListFragment extends Fragment implements LoaderManager.Loade
 			EpisodeCursor episode = new EpisodeCursor(_cursor);
 
 			holder.container.setTag(episode.getId());
-            holder.title.setText(episode.getTitle());
-            holder.date.setText(context.getString(R.string.released_on) + " " + _pubDateFormat.format(episode.getPubDate()));
-            if (episode.getDuration() > 0) {
-                holder.duration.setText(Helper.getVerboseTimeString(context, episode.getDuration() / 1000f, false) + " " + context.getString(R.string.in_duration));
-                holder.duration.setVisibility(View.VISIBLE);
-            } else
-                holder.duration.setVisibility(View.GONE);
-            holder.play.setTag(episode.getId());
-            holder.playlist.setTag(R.id.episodeId, episode.getId());
+			holder.title.setText(episode.getTitle());
+			holder.date.setText(context.getString(R.string.released_on) + " " + _pubDateFormat.format(episode.getPubDate()));
+			if (episode.getDuration() > 0) {
+				holder.duration.setText(Helper.getVerboseTimeString(context, episode.getDuration() / 1000f, false) + " " + context.getString(R.string.in_duration));
+				holder.duration.setVisibility(View.VISIBLE);
+			} else
+				holder.duration.setVisibility(View.GONE);
+			holder.play.setTag(episode.getId());
+			holder.playlist.setTag(R.id.episodeId, episode.getId());
 
-            Integer inPlaylist = episode.getPlaylistPosition();
-            if (inPlaylist == null) {
-                holder.playlist.setTag(R.id.playlist, Integer.MAX_VALUE);
-                holder.playlist.setText(R.string.add_to_playlist);
-            } else {
-                holder.playlist.setTag(R.id.playlist, null);
-                holder.playlist.setText(R.string.remove_from_playlist);
-            }
+			Integer inPlaylist = episode.getPlaylistPosition();
+			if (inPlaylist == null) {
+				holder.playlist.setTag(R.id.playlist, Integer.MAX_VALUE);
+				holder.playlist.setText(R.string.add_to_playlist);
+			} else {
+				holder.playlist.setTag(R.id.playlist, null);
+				holder.playlist.setText(R.string.remove_from_playlist);
+			}
 
 			if (episode.isDownloaded(context))
 				holder.play.setText(R.string.play);
@@ -294,10 +318,10 @@ public class EpisodeListFragment extends Fragment implements LoaderManager.Loade
 		}
 
 		@Override
-        public long getItemId(int position) {
+		public long getItemId(int position) {
 			_cursor.moveToPosition(position);
-            return new EpisodeCursor(_cursor).getId();
-        }
+			return new EpisodeCursor(_cursor).getId();
+		}
 
 		@Override
 		public int getItemCount() {
